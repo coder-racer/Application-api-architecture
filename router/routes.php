03b5fc235846d6d273f240cf6353c2bc2bfe61ef
atom <vlad.v428@gmail.com>
2021-11-14 23:15:48 +0600

    Auth router "get" to "post"

--- router/routes.php
+++ router/routes.php
@@ -1,7 +1,7 @@
 <?php
 
-    $router->get('/login', 'auth@login');
-    $router->get('/register', 'auth@register');
+    $router->post('/login', 'auth@login');
+    $router->post('/register', 'auth@register');
 
     $router->get("/", function (){
         echo 'Welcome!';
